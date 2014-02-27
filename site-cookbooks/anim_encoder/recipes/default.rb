f64694fc2e7e6bde446f3e530f91498cb46fa655
Fujisawa Shin <fujisawa-shin@kayac.com>
2014-02-27 21:27:23 +0900

    anim_encoderのチェックアウトまで。

--- site-cookbooks/anim_encoder/recipes/default.rb
+++ site-cookbooks/anim_encoder/recipes/default.rb
@@ -7,6 +7,13 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# config
+
+username = node["username"]
+checkout_path = "/home/#{username}/anim_encoder"
+
+# install packages
+
 %w{
   pngcrush
   python-opencv
@@ -16,4 +23,13 @@
   package pkg do 
     action :install
   end
+end
+
+
+# checkout anim_encodeer
+
+git checkout_path do
+  repository "git://github.com/nobii/anim_encoder.git"
+  reference "master"
+  action :sync
 end
